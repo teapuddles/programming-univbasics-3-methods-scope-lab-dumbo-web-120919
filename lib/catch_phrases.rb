8253883221c7388d0a49e3be314dfa27b0af7c01
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-25 18:33:31 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,7 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase = "It's-a me, Mario!"
 end
 
 def toadstool
-  puts status
+  puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
